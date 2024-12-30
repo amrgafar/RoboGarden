e7cd9a72cc23d0090ac7daf96e04c2db95e1b9c0
Amr Gafar <62294242+amrgafar@users.noreply.github.com>
2024-12-31 00:36:47 +0200

    Create second.py

---
+++ second.py
@@ -0,0 +1,10 @@
+def print_introduction():
+    name = "Amr Gafar"
+    age = 24
+    learning_goal = "I want to learn how to build Python projects that solve real-world problems and enhance my programming skills."
+
+  print(f"Name: {name}")
+    print(f"Age: {age}")
+    print(f"Learning Goal: {learning_goal}")
+
+print_introduction()
