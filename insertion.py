0f392228f3068a0db4710c0b5bdb63f1702955d0
Amr Gafar <62294242+amrgafar@users.noreply.github.com>
2025-01-01 07:32:16 -0800

    Create insertion.py

---
+++ insertion.py
@@ -0,0 +1,21 @@
+def insertion_sort(arr):
+    # Traverse through 1 to len(arr)
+    for i in range(1, len(arr)):
+        key = arr[i]
+        # Move elements of arr[0..i-1], that are greater than key,
+        # to one position ahead of their current position
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+
+def main():
+    arr = [64, 34, 25, 12, 22, 11, 90]
+    print("Original array:", arr)
+
+    insertion_sort(arr)
+
+    print("Sorted array:", arr)
+
+main()
